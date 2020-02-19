69a019a05c48abfea524ff4d2e7f107ced398245
abishek <ashek70@gmail.com>
2020-02-19 01:36:14 -0500

    working -GET and PUT

--- client.py
+++ client.py
@@ -3,54 +3,61 @@
 """
 Client 
 """
-import socket, requests
+import socket, requests, sys
 
 
 def create_request(request_type, file_name, host):
     return """%s /%s HTTP/1.1\r\nHost:%s\r\n\r\n""" %(request_type, file_name, host)
     
 
-# def init():
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#         s.connect((host, PORT))
-#         # print(create_request(request_type, file_name, host))
-#         s.sendall(create_request(request_type, file_name, host).encode())
-#         data = s.recv(1024)
-#     print('Received', repr(data))
-
-
 def do_get(s, file_name, host):
     s.sendall(create_request("GET", file_name, host).encode())
 
-def do_post(s, file_name, host):
-    req_head = create_request("POST", file_name, host).encode()
-    f = open(file_name, 'rb')
-    req_data = f.read()
-    f.close()
-    req_head += req_data
-    s.sendfile(file_name)
+def do_PUT(s, file_name, host):
+    req_head = create_request("PUT", file_name, host).encode()
+    s.sendall(req_head)
+
 
 
 if __name__ == "__main__":
-    host = '127.0.0.1'    # Server addr
-    port = 4000           # Server port
-    #request_type = "GET"
-    file_name = 'public/index.html'
 
-    request_type ="POST"
+    ###########################
+    host = sys.argv[1]    # Server addr
+    port = int(sys.argv[2])           # Server port
+    request_type = sys.argv[3]
+    file_name = sys.argv[4]
+    print(host, port, request_type, file_name)
+    #print(""" server host %s \nserver port %s\n """ )
+    ###############################
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         ###########################
         if request_type =="GET":
             do_get(s, file_name, host)
+            data = s.recv(1024)
+            print("Server Response: ", data)
 
-        elif request_type =="POST":
-            do_post(s, file_name, host)
+        elif request_type =="PUT":
+            #while True:
+            do_PUT(s, file_name, host)
+            while True:
+                data = s.recv(1024)
+                print("PUT Response data ", data)
+                if not data:
+                    print("No data!")
+                    break
+                with open(file_name, 'rb') as f:
+                    #s.send(f)
+                    s.sendfile(f)
+                break
+            data = s.recv(1024)
+            print(data)
+                    
         else:
             print("INVALID Request Type")
         # print(create_request(request_type, file_name, host))
         #s.sendall(create_request(request_type, file_name, host).encode())
-        data = s.recv(1024)
-        print('Received', repr(data))
+        # data = s.recv(1024)
+        # print('Received', repr(data))
     
     
--- server.py
+++ server.py
@@ -19,24 +19,101 @@ class MyServer(object):
             client.settimeout(50)
             print('Connected by client ', address)
             threading.Thread(target=self._handle_client, args=(client, address)).start()
-    
+
+    def _handle_PUT(self, data, client, file_requested):
+        # while True:
+            # data = client.recv(1024)
+        #print(data)
+        try:
+            # f = open(file_requested, 'w')
+            # f.write(data)
+            #f.close()
+            response_header = self._generate_headers(200)
+            response = response_header.encode()
+        except Exception as e:
+            print("__PUT func", e)
+            response_header = self._generate_headers(404)
+            response = response_header.encode()
+        client.send(response)
+
+
+    def _handle_GET(self, data, client, file_requested):
+        try:
+            #print("file--", file)
+            f = open(file_requested, 'rb')
+            response_data = f.read()
+            f.close()
+            response_header = self._generate_headers(200)
+            response = response_header.encode()
+            response += response_data
+        except Exception as e:
+            print("_handle_GET error: File not found !", e)
+            response_header = self._generate_headers(404)
+            response = response_header.encode()
+        print("Response header: ", response)
+        client.send(response)
+        client.close() 
+
+
     def _handle_client(self, client, address):
+        self.current_req_state = None
+        self.file_requested = None
         try:
             # self.s.bind((self.host, self.port))
             # self.s.listen()
             # self.conn, self.addr = self.s.accept()
             # print('Connected by client ', self.addr)
-            #while True:
-            data = client.recv(1024)
-            if not data:
-                print("NO DATAAA")
-                client.close()
-                #break
-            self._handle_data(data, client)
-            client.close()
+            while True:
+                #print("______ self.current_req_state____", self.current_req_state)
+                data = client.recv(1024)
+                print("+++++ Data recv ++++++", data)
+                if not data:
+                    # if self.current_req_state =="PUT":
+                    #     response_header = self._generate_headers(201)
+                    #     response = response_header.encode()
+                    #     client.send(response)
+                    self.current_req_state = None
+                    print("Connection closed by client ", address)
+                    client.close() 
+                    break
+                if self.current_req_state =="PUT":
+                    print("PUT DATA ->>>>>>", data.decode())
+                    f = open(self.file_requested, 'wb')
+                    f.write(data)
+                    f.close()
+                    response_header = self._generate_headers(201)
+                    response = response_header.encode()
+                    client.send(response)
+                    client.close()
+                try:
+                    parsed_req = data.decode().split()
+                    # print(parsed_req)
+                    self.file_requested =  (parsed_req[1].split('?')[0]).split('/')[1]
+                except:
+                    pass
+                #file_requested = os.path.join(self.public_dir, file_requested.split('/')[1])
+                #file_requested = file_requested.split('/')[1]
+                print("file_requested: ", self.file_requested)
+                if parsed_req[0] == "GET":
+                    self.current_req_state = "GET"
+                    print("GET Request received from client for %s" %(self.file_requested))
+                    self._handle_GET(data, client, self.file_requested)
+                    break                
+                elif(parsed_req[0] == "PUT"):
+                    self.current_req_state = "PUT"
+                    print("PUT Request received from client for %s" %(parsed_req[1]))
+                    self._handle_PUT(data, client, self.file_requested)
+                else:
+                    pass
+                    # print("Wrong Request")
+                    # response_header = self._generate_headers(404)
+                    # client.send(response_header)
+                    # client.close()
+            #client.close()
         except Exception as e:
-            print("ERROR!!", e)
+            print("_handle_client ERROR!!", e)
  
+
     def _generate_headers(self, response_code):
         """ Generate HTTP response headers """
         header = ''
@@ -44,6 +121,10 @@ class MyServer(object):
             header += 'HTTP/1.1 200 OK\n'
         elif response_code == 404:
             header += 'HTTP/1.1 404 Not Found\n'
+        elif response_code == 201:
+            header += 'HTTP/1.1 200 OK File Created\n'
+        else:
+            header += 'HTTP/1.1 400 Bad Request\n'
 
         time_now = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
         header += 'Date: {now}\n'.format(now=time_now)
@@ -55,60 +136,11 @@ class MyServer(object):
     #     return
     #     #self.conn.close()
 
-    def _handle_data(self, data, client):
-        parsed_req = data.decode().split()
-        # print(parsed_req)
-        file_requested =  parsed_req[1].split('?')[0]
-        file_requested = os.path.join(self.public_dir, file_requested.split('/')[1])
-        print("file_requested: ", file_requested)
-        if parsed_req[0] == "GET":
-            print("GET Request received from client for %s" %(parsed_req[1]))
-            try:
-                #print("file--", file)
-                f = open(file_requested, 'rb')
-                if parsed_req[0] == "GET":
-                    response_data = f.read()
-                f.close()
-                response_header = self._generate_headers(200)
-                response = response_header.encode()
-                response += response_data
-            except:
-                print("File not found !")
-                response_header = self._generate_headers(404)
-                response = response_header.encode()
-        
-        elif(parsed_req[0] == "POST"):
-            print("POST Request received from client for %s" %(parsed_req[1]))
-            try:
-                print("file--", parsed_req)
-                f = open(file_requested, 'w')
-                f.write("")
-                f.close()
-                response_header = self._generate_headers(200)
-                response = response_header.encode()
-            except:
-                print("File not written !")
-                response_header = self._generate_headers(404)
-                response = response_header.encode()
-        else:
-            print("Wrong Request")
-            response_header = self._generate_headers(404)
-
-        print(response)
-        # response = response_header.encode()
-        # if parsed_req[0] == "GET":
-        #     response += response_data
-        #self._send_data(response)
-        client.send(response)
-        client.close()
-        return
-    
-   
-
 
 if __name__ == "__main__":
-    HOST = '127.0.0.1'  
-    PORT = 4000
+    import sys
+    HOST = sys.argv[1] #'127.0.0.1'  
+    PORT = int(sys.argv[2]) #4000
     ms = MyServer(HOST, PORT)
     ms.start()
 
